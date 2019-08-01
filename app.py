20f9b35999adbd342a161ba0f49e3098fcc71962
admin <you@example.com>
2019-08-01 22:43:54 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from venv import logger
+
+from flask import Flask, request, Response
+from viberbot.api.messages import TextMessage
+from viberbot.api.viber_requests import ViberMessageRequest, ViberSubscribedRequest, ViberFailedRequest
+
+from viber import viber
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/wizl_test', methods=['POST', 'GET'])
+def wizl_test():
+    logger.debug("received request. post data: {0}".format(request.get_data()))
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+        # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+    return Response(status=200)
+    # return 'Hello World!'
+    # return Response(status=200)
+
+
+if __name__ == '__main__':
+    context = ('server.crt', 'server.key')
+    app.run(host='192.168.1.65', debug=True, port=5000)
+    # flask run -h 192.168.1.65  # lauch local server
+    # app.run(host='0.0.0.0', port=443, debug=False, ssl_context=context)
+    # app.run()
---
+++ viber.py
@@ -0,0 +1,15 @@
+from viberbot import BotConfiguration, Api
+from viberbot.api.messages import TextMessage
+
+bot_configuration = BotConfiguration(
+    name='Mihail',
+    avatar='http://sun9-29.userapi.com/c824201/v824201969/173425/UZNGRozhtic.jpg?ava=1',
+    auth_token='4a1218ec19e7d382-bc52008d33eea8c2-e819c1e1cd8babcd'
+)
+
+viber = Api(bot_configuration)
+# viber.send_messages(viber.get_online([1]),TextMessage(text="thanks for subscribing!"))
+# print(viber.get_account_info())
+# viber.set_webhook("https://wizl.me/")
+
+
